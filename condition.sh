59dbcd19bcc1ca4fd38827cca2e13b4a8b1ecd39
rupaak <rupakt525@gmail.com>
2021-09-20 00:09:38 +0545

    is else elif

---
+++ condition.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+#if statement structure
+# if [condition]
+# then
+#     statement
+# fi
+
+# count=10
+# if [ $count -lt 20 ]
+# then
+#     echo true
+# fi
+
+#OR
+
+# if (($count < 20))
+# then    
+#     echo true
+# fi
+
+#string comparision
+# a=c
+# b=d
+# if [[ $a < $b ]]
+# then
+#     echo equal
+# fi
+
+# for angle sign (< , > , <=, >=) in string comparision use double brackets either (()) or [[]] for comparsion
+
+
+#if else
+# if [condition]
+# then
+#     statement
+# else   
+#     statement
+# fi
+
+# a=5
+# b=6
+# if [ $a -gt $b ]
+# then 
+#     echo hi
+# else    
+#     echo bye
+# fi
+
+# elif
+# if [condition]
+# then 
+#     statement
+# elif [condition]
+# then
+#     statement
+# else
+#     statement
+# fi
